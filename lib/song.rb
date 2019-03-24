1fb2fa47a36b842f5d74c31bff5ef479b3800d66
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-24 18:23:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 class Song
 attr_accessor :name, :genre, :artist
 @@all = []
-  def initialize(name, genre)
+  def initialize(name, artist, genre)
     @name = name
     @genre = genre
+    @@all << self
   end
 
   def self.all
-
+    
   end
 
 end
